0cc4bd4df69cc52709479d751bf5b8f70a2ed936
Meshpy <209317@edu.p.lodz.pl>
2019-10-08 17:28:48 +0200

    Add service for API

---
+++ src/main/java/com/ppkwu/lab2/endpoint/StringUtilityEndpoint.java
@@ -0,0 +1,11 @@
+package com.ppkwu.lab2.endpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class StringUtilityEndpoint {
+
+
+}
---
+++ src/main/java/com/ppkwu/lab2/service/StringUtilityService.java
@@ -0,0 +1,28 @@
+package com.ppkwu.lab2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StringUtilityService {
+
+    public boolean hasUpperCaseLetters(String text) {
+        return !text.equals(text.toLowerCase());
+    }
+
+    public boolean hasLowerCaseLetters(String text) {
+        return text.equals(text.toLowerCase());
+    }
+
+    public boolean hasAnySpecialSymbols(String text) {
+        return text.chars().allMatch(Character::isLetter);
+    }
+
+    public boolean hasAnyWhiteSpace(String text) {
+        return text.contains(" ");
+    }
+
+    public int getLengthOfText(String text) {
+        return text.length();
+    }
+
+}
